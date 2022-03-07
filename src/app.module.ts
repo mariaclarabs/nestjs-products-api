c66c23f09ae16ef45a913f9139b117f27d37b0ee
mariaclarabs <clarabsouza@hotmail.com>
2022-03-07 14:24:48 -0300

    style: arrange files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './products.controller';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService],
+  controllers: [ProductsController],
+  providers: [],
 })
 export class AppModule {}
